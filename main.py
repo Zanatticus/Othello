eac82cf89e1a95f4b6bba75c87fdf5a146b2a3b5
Zanatticus <zingare@live.com>
2022-06-07 18:20:54 -0400

    Test

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+board = Tk()
+frame = ttk.Frame(board, padding=10)
+frame.grid()
+ttk.Label(frame, text="Othello").grid(column=1, row=0)
+ttk.Button(frame, text="End Game", command=board.destroy).grid(column=1, row=1)
+board.mainloop()
